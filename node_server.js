b92ef06b865ba70f1c0eec5a13f3c6e5cd33de2e
kelinn <36924700+kelinn@users.noreply.github.com>
2020-09-11 17:34:37 -0600

    caso2
    fix del server

--- node_server.js
+++ node_server.js
@@ -1,12 +1,23 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
-http.createServer(function (req, res) {
-  fs.readFile('index3.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
-console.log('Server is running');
-    
+http.createServer(function(req, res){
+
+    if(req.url === "/"){
+        fs.readFile("./index3.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.css$")){
+        var cssPath = path.join(__dirname, '/', req.url);
+        var fileStream = fs.createReadStream(cssPath, "UTF-8");
+        res.writeHead(200, {"Content-Type": "text/css"});
+        fileStream.pipe(res);
+
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+}).listen(3000);
